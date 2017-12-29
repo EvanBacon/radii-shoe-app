dcfb815594d33e0c6b8216b948c058c346f5fbbb
Evan Bacon <baconbrix@gmail.com>
2017-12-29 01:11:55 -0600

    updated format

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import List from './screens/List';
 import Details from './screens/Details';
-import { StackNavigator } from 'react-navigation'; // 1.0.0-beta.22
+import { StackNavigator } from 'react-navigation';
 
 export default StackNavigator({
   List: { screen: List },
